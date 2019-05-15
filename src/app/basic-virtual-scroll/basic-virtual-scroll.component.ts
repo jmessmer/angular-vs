bd6d6cfff573a81f4d97fa205d163b098bcd52e0
jmessmer <mezlight@gmail.com>
2019-05-15 10:43:12 -0400

    1 file modified

--- src/app/basic-virtual-scroll/basic-virtual-scroll.component.ts
+++ src/app/basic-virtual-scroll/basic-virtual-scroll.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import * as faker from 'faker';
 import { CommerceProductService } from '../service/commerce-product.service';
 import { Product } from '../service/product';
 
@@ -12,20 +11,7 @@ import { Product } from '../service/product';
 export class BasicVirtualScrollComponent implements OnInit {
   products: Product[];
 
-  constructor(private commerceProductService: CommerceProductService) {
-    /*this.products = Array(100)
-      .fill(1)
-      .map(_ => {
-        return {
-          image: faker.image.food(),
-          name: faker.commerce.productName(),
-          description: faker.lorem.sentence(),
-          price: faker.commerce.price(1, 100),
-          weight: faker.random.number({ min: 1, max: 10, precision: 0.01 }),
-          quantity: faker.random.number({ min: 0, max: 5 }),
-        };
-      });*/
-  }
+  constructor(private commerceProductService: CommerceProductService) { }
 
   getProducts(): void {
     this.products = this.commerceProductService.getProducts(100);

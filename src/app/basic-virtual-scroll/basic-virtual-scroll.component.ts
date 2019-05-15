32fb7486c32cbd63d11031495765a8ad9ce393bf
jmessmer <mezlight@gmail.com>
2019-05-14 22:17:53 -0400

    1 file modified

--- src/app/basic-virtual-scroll/basic-virtual-scroll.component.ts
+++ src/app/basic-virtual-scroll/basic-virtual-scroll.component.ts
@@ -9,16 +9,22 @@ import * as faker from 'faker';
 })
 export class BasicVirtualScrollComponent implements OnInit {
   people;
-  
+
   constructor() {
-    this.people = Array(1000)
+    this.people = Array(100)
       .fill(1)
       .map(_ => {
         return {
-          name: faker.name.findName(),
-          bio: faker.hacker.phrase(),
-          image: faker.image.cats(),
-          emoji: emojiRandom()
+          //name: faker.name.findName(),
+         // bio: faker.hacker.phrase(),
+          //image: faker.image.cats(),
+          //emoji: emojiRandom(),
+          image: faker.image.food(),
+          name: faker.commerce.productName(),
+          description: faker.lorem.sentence(),
+          price: faker.commerce.price(1, 100),
+          weight: faker.random.number({ min: 1, max: 10, precision: 0.01 }),
+          quantity: faker.random.number({ min: 0, max: 5 }),
         };
       });
   }

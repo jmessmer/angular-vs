f1aa581bdda1d2199ce24a1ff1cb97625b36e9ef
Jason Messmer <mezlight@gmail.com>
2019-05-15 13:06:28 -0400

    Add files via upload

---
+++ src/app/ag-grid-scroll/ag-grid-scroll.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import {GridOptions} from "ag-grid";
+import * as faker from 'faker';
+
+@Component({
+  selector: 'app-ag-grid-scroll',
+  templateUrl: './ag-grid-scroll.component.html',
+  styleUrls: ['./ag-grid-scroll.component.css']
+})
+export class AgGridScrollComponent implements OnInit {
+    private gridOptions: GridOptions;
+    private workers;
+
+  constructor() {
+    this.gridOptions = <GridOptions>{};
+        this.gridOptions.columnDefs = [
+            {
+                headerName: "Worker",
+                field: "name",
+                width: 200
+            }
+        ];
+      this.gridOptions.pagination = true;
+      this.workers = Array(200)
+      .fill(1)
+      .map(_ => {
+        return {
+          name: faker.name.lastName() + ", " + faker.name.firstName()
+        };
+      });
+      this.gridOptions.rowData = this.workers;
+   }
+
+  ngOnInit() {
+  }
+
+}

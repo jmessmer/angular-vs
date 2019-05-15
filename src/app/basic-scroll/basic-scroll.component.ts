e6c5d5e82c467a1e12af29eb46493dfe069540b6
jmessmer <mezlight@gmail.com>
2019-05-15 11:18:16 -0400

    4 files modified, 1 added, 1 deleted

--- src/app/basic-scroll/basic-scroll.component.ts
+++ src/app/basic-scroll/basic-scroll.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { CommerceProductService } from '../service/commerce-product.service';
+import { Product } from '../service/product';
 
 @Component({
   selector: 'app-basic-scroll',
   templateUrl: './basic-scroll.component.html',
-  styleUrls: ['./basic-scroll.component.css']
+  styleUrls: ['./basic-scroll.component.scss']
 })
 export class BasicScrollComponent implements OnInit {
+  products: Product[];
 
-  constructor() { }
+  constructor(private commerceProductService: CommerceProductService) { }
+
+  getProducts(): void {
+    this.products = this.commerceProductService.getProducts(100);
+  }
 
   ngOnInit() {
+    this.getProducts();
   }
 
 }

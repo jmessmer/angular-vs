76f6ecde949c5680b5f09a8b90b952eeba622a5d
jmessmer <mezlight@gmail.com>
2019-05-16 09:45:50 -0400

    2 files modified, 1 added

--- src/app/dashboard-card/dashboard-card.component.ts
+++ src/app/dashboard-card/dashboard-card.component.ts
@@ -1,11 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { trigger, keyframes, animate, transition } from '@angular/animations';
+import * as kf from '../keyframes';
+import { Dashboard } from '../dashboard/dashboard';
 
 @Component({
   selector: 'app-dashboard-card',
   templateUrl: './dashboard-card.component.html',
-  styleUrls: ['./dashboard-card.component.scss']
+  styleUrls: ['./dashboard-card.component.scss'],
+  animations: [
+    trigger('cardAnimator', [
+      transition('* => wobble', animate(1000, keyframes(kf.wobble))),
+      transition('* => swing', animate(1000, keyframes(kf.swing))),
+      transition('* => jello', animate(1000, keyframes(kf.jello))),
+      transition('* => zoomOutRight', animate(1000, keyframes(kf.zoomOutRight))),
+      transition('* => slideOutLeft', animate(1000, keyframes(kf.slideOutLeft))),
+      transition('* => rotateOutUpRight', animate(1000, keyframes(kf.rotateOutUpRight))),
+      transition('* => flipOutY', animate(1000, keyframes(kf.flipOutY))),
+    ])
+  ]
 })
 export class DashboardCardComponent implements OnInit {
+
+  @Input() dashboard: Dashboard;
+
+  animationState: string;
+
+  startAnimation(state) {
+    if (!this.animationState) {
+      this.animationState = state;
+    }
+  }
+
+  resetAnimationState() {
+    this.animationState = '';
+  }
 
   constructor() { }
 
